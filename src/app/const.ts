b59add25e4c056f7a081a5e3339e818fa488f4ca
Tristan Matthias <hello@tristanmatthias.com>
2019-01-28 14:19:20 +1100

    fix(app): fixed broken SERVER_API constant

--- src/app/const.ts
+++ src/app/const.ts
@@ -1,4 +1,4 @@
-export const SERVER_API = 'http://localhost:9999/api/v1';
+export const SERVER_API = '/api/v1';
 export const BASE_URI = '/admin';
 export const LS_JWT = 'token';
 export const LS_EMAIL = 'email';
